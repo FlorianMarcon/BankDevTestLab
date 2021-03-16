296948aa81a99f5e383fd588fbd5075decb1dfbd
FlorianMarcon <florian.marcon@epitech.eu>
2021-03-16 15:58:50 +0100

    Update CadeoCompleteDeployment

--- Artifacts/CadeoCompleteDeployment/linux_install_cadeo_v1.sh
+++ Artifacts/CadeoCompleteDeployment/linux_install_cadeo_v1.sh
@@ -39,6 +39,12 @@ echo "\e[33mLaunching script to start docker compose in folder $pwd\e[0m"
 chmod +x "./init-letsencrypt.sh"
 ./init-letsencrypt.sh
 
+initStatus=$(echo $?)
+if [ $installationStatus -ne 0 ] ; then
+    echo "Error while lets encrypt initialisation"
+    exit 42
+fi
+
 echo "\e[32mArtifact finished successfully\e[0m"
 exit 0
 
