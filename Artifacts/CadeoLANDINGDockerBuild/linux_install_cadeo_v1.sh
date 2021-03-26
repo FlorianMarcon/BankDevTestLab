0e6db3b5a81d1e8b60dfdeddffaabdc35ffb794b
FlorianMarcon <florian.marcon@epitech.eu>
2021-03-26 19:55:50 +0000

    Fix error about args

--- Artifacts/CadeoLANDINGDockerBuild/linux_install_cadeo_v1.sh
+++ Artifacts/CadeoLANDINGDockerBuild/linux_install_cadeo_v1.sh
@@ -30,7 +30,7 @@ cd Cadeo_Front_Web
 # wget $4 -O .env
 
 echo  "\e[33mBuilding Image\e[0m"
-docker build -t cadeo-landing-$5 .
+docker build -t cadeo-landing-$4 .
 ret=$?
 echo "\e[32mArtifact finished successfully\e[0m"
 exit ret
